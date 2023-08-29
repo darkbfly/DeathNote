356e4ae4772667eb22ed86b4223bb9d6e1d4622d
xuwb <xuwb@superb-pay.com>
2023-08-29 14:37:38 +0800

    akrd

---
+++ updateOtherGit.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+
+call :DownloadAndRename "https://raw.githubusercontent.com/smallfawn/QLScriptPublic/main/yingsheng.js" "yingsheng.js" "ys.js"
+call :DownloadAndRename "https://raw.githubusercontent.com/smallfawn/QLScriptPublic/main/akrd.js" "akrd.js" "akrd.js"
+
+exit /b
+
+:DownloadAndRename
+rem 参数1：要下载的文件URL
+rem 参数2：保存路径
+rem 参数3：新文件名
+
+set "url=%~1"
+set "outputPath=%~2"
+set "newName=%~3"
+
+curl -o "%outputPath%" "%url%"
+
+if %errorlevel% neq 0 (
+    echo 下载失败！
+    exit /b
+)
+
+if "%newName%" neq "%outputPath%" (
+    del "%newName%"
+    rename "%outputPath%" "%newName%"
+    echo 文件已下载并重命名为 %newName%
+) 
+exit /b
